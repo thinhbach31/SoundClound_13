64f95e005687a09fc0bbf46b8704961d6b802f2e
unknown <thinhzoro96@gmail.com>
2018-08-16 16:10:13 +0700

    playing activity

---
+++ app/src/main/java/com/example/admin/scloud/screen/playing/PlayingActivity.java
@@ -0,0 +1,15 @@
+package com.example.admin.scloud.screen.playing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.admin.s_cloud.R;
+
+public class PlayingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playing);
+    }
+}
