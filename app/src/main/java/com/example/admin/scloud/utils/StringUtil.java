e65c7d82a1a66b0b03f21700318c5e2612da3a27
unknown <thinhzoro96@gmail.com>
2018-08-22 17:18:03 +0700

    xmll and string

--- app/src/main/java/com/example/admin/scloud/utils/StringUtil.java
+++ app/src/main/java/com/example/admin/scloud/utils/StringUtil.java
@@ -1,9 +1,12 @@
 package com.example.admin.scloud.utils;
 
 
+import android.annotation.SuppressLint;
+
 import com.example.admin.s_cloud.BuildConfig;
 
 public class StringUtil {
+    @SuppressLint("DefaultLocale")
     public static String convertUrlFetchMusicGenre(String genre, int limit, int offset) {
         return String.format("%s%s%s&%s=%s&%s=%d&%s=%d", ConstantNetwork.BASE_URL,
                 ConstantNetwork.PARA_MUSIC_GENRE, genre, ConstantNetwork.CLIENT_ID,
@@ -20,6 +23,7 @@ public class StringUtil {
         return String.format("%s?%s=%s", url, ConstantNetwork.CLIENT_ID, BuildConfig.API_KEY);
     }
 
+    @SuppressLint("DefaultLocale")
     public static String convertUrlSearchTrack(String trackName, int offset) {
         return String.format("%s%s%s&%s=%d&%s=%s", ConstantNetwork.BASE_URL,
                 ConstantNetwork.PARA_SEARCH_TRACK, trackName,
